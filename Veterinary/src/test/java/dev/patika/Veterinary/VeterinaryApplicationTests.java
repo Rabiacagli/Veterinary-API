4ac86fadc4809a7ad278b3ff6387cefcf2070772
Rabiacagli <rabiaacgl@gmail.com>
2024-02-26 20:47:24 +0300

    v1

---
+++ Veterinary/src/test/java/dev/patika/Veterinary/VeterinaryApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.patika.Veterinary;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VeterinaryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
