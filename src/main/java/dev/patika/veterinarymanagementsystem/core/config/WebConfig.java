42aaadfebd64478dd359146daa9b0ef7bfeab8c9
Rabiacagli <rabiaacgl@gmail.com>
2024-06-09 00:59:48 +0300

    Veterinary Project V1.1

--- src/main/java/dev/patika/veterinarymanagementsystem/core/config/WebConfig.java
+++ src/main/java/dev/patika/veterinarymanagementsystem/core/config/WebConfig.java
@@ -10,7 +10,7 @@ public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOrigins("https://veterinarymanagementapp.netlify.app/")
+                .allowedOrigins("https://veterinarysystem.netlify.app/")
                 .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
                 .allowedHeaders("*")
                 .allowCredentials(true);
