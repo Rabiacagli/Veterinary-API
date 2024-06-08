42879a35df8004e64708461ade80debc8f154fb1
Rabiacagli <rabiaacgl@gmail.com>
2024-06-09 00:26:36 +0300

    Project updated

--- Veterinary/src/main/java/dev/patika/Veterinary/core/exception/ConflictException.java
+++
@@ -1,7 +0,0 @@
-package dev.patika.Veterinary.core.exception;
-
-public class ConflictException extends RuntimeException{
-    public ConflictException(String message) {
-        super(message);
-    }
-}
--- Veterinary/src/main/java/dev/patika/Veterinary/core/utilies/Msg.java
+++
@@ -1,9 +0,0 @@
-package dev.patika.Veterinary.core.utilies;
-
-public class Msg {
-    public static  final String CREATED = "New record created";
-    public static  final String VALIDATE_ERROR = "Data validation error!";
-    public static  final String OK = "Transaction successful";
-    public static  final String NOT_FOUND = "Data not found!";
-
-}
